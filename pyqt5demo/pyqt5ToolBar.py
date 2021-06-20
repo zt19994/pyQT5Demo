957e0b82e577ce58ae23199e5034ad85a4c501c7
zt1994 <zt191610942@163.com>
2021-06-20 17:01:03 +0800

    ToolBar

--- pyqt5demo/pyqt5ToolBar.py
+++ pyqt5demo/pyqt5ToolBar.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# @Time    : 2021/6/20 16:49
+# @Time    : 2021/6/20 16:57
 # @Author  : zt
 
 import sys
@@ -14,30 +14,21 @@ class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         # 界面绘制交给InitUI方法
+        self.toolbar = self.addToolBar('Exit')
         self.init_ui()
 
     def init_ui(self):
-        # 创建一个菜单栏和一个菜单。这个菜单将终止应用程序。Ctrl + Q的行动是可访问的快捷方式。
-        # QAction可以操作菜单栏,工具栏,或自定义键盘快捷键。
-        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
+        # 创建一个简单的工具栏。工具栏有有一个按钮,点击关闭窗口
+        exitAction = QAction(QIcon('exit2.png'), 'Exit', self)
         exitAction.setShortcut('Ctrl+Q')
-        # 创建一个鼠标指针悬停在该菜单项上时的提示
-        exitAction.setStatusTip('Exit application')
         # 点击菜单的时候，调用qApp.quit,终止应用程序
         exitAction.triggered.connect(qApp.quit)
 
-        self.statusBar()
-
-        # 创建一个菜单栏
-        menubar = self.menuBar()
-        # 添加菜单
-        fileMenu = menubar.addMenu('&File')
-        # 添加事件
-        fileMenu.addAction(exitAction)
+        self.toolbar.addAction(exitAction)
         # 设置窗口大小
         self.setGeometry(300, 300, 300, 200)
         # 设置窗口的标题
-        self.setWindowTitle('Menubar')
+        self.setWindowTitle('Toolbar')
         # 显示窗口
         self.show()
 
